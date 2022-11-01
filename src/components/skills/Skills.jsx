fd4f1a66898a7adead78b509b1ef12383432972a
Rainer Regan <rainercoc@gmail.com>
2022-11-01 10:18:12 +0700

    Fixing bugs

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -19,7 +19,7 @@ function Skills() {
                 <Line color='white' />
             </div>
 
-            <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 w-full gap-4 mt-8'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 w-full gap-4 mt-8'>
                 <SkillTab
                     icon={<FaLaravel />}
                     title="Laravel"
